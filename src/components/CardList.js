df6f6f9ccd9c4dc0a4d1adec59338197379cd912
crux-in-lan <nicksona.in.lan@gmail.com>
2019-03-23 12:30:38 +0200

    Done loading robots from API and filter them by text

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = (props) => {
-	const {sampleRobots} = props;
+	const {filteredRobots} = props;
 	return (
 		<div>
 			{
-				sampleRobots.map(robot => {
-					return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} imageUrl={robot.image}/>
+				filteredRobots.map(robot => {
+					return <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} imageUrl={`https://robohash.org/${robot.id}`}/>
 				})
 			}
 		</div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -15,27 +15,31 @@ class App extends Component {
       robots: []
     }
   }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value});
+  }
+
+  componentDidMount = () => {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+    .then(response => response.json())
+    .then(result => {
+      this.setState({robots: result});
+    })
+    .catch(err => alert('Unable to fetch robots from API')) 
+  }
+
   render() {
-    const sampleRobots = [
-      {
-        image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/H/K9/HK962/HK962?wid=1144&hei=1144&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1467071336284',
-        name: 'Nikola',
-        email: 'nicksona.in.lan@gmail.com',
-        id: 1
-      },
-      {
-        image: 'https://cdn.babymarkt.com/babymarkt/img/549456/443/tomy-lamaze-play-grow-rusty-robot-a059936.jpg',
-        name: 'Ivan',
-        email: 'ivan.in.lan@gmail.com',
-        id: 2
-      }
-    ];
+    const filteredRobots = this.state.robots.filter(robot => {
+      return (robot.name.includes(this.state.searchField) ||
+        robot.email.includes(this.state.searchField))
+    });
     return (
       <div className="App">
-        <SearchBox/>
+        <SearchBox onSearchChange={this.onSearchChange}/>
         <Scrollable>
          <ErrorBondry>
-            <CardList sampleRobots={sampleRobots}/>
+            <CardList filteredRobots={filteredRobots}/>
           </ErrorBondry>
         </Scrollable>
       </div>
